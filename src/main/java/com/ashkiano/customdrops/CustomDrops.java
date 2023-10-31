376a8cbd825a11c5deb381ad8214f1f0de64d71f
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2023-10-31 09:48:59 +0100

    version 1.3

--- src/main/java/com/ashkiano/customdrops/CustomDrops.java
+++ src/main/java/com/ashkiano/customdrops/CustomDrops.java
@@ -11,5 +11,7 @@ public class CustomDrops extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new MobDropListener(this), this);
 
         Metrics metrics = new Metrics(this, 19462);
+
+        this.getLogger().info("Thank you for using the CustomDrops plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
     }
 }
